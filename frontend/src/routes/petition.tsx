798461ed1dfa5fbc7ae283bc65c3c430977f8a5d
Ezra Mitchell <ezra.john.mitchell@gmail.com>
2025-04-26 12:14:16 -0400

    adding dynamic cropping

--- frontend/src/routes/petition.tsx
+++ frontend/src/routes/petition.tsx
@@ -4,21 +4,31 @@ import { Label } from "@/components/ui/label";
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import Markdown from "react-markdown";
+import { type Crop, PercentCrop, PixelCrop, ReactCrop } from "react-image-crop"
 
 export const Route = createFileRoute("/petition")({
   component: Petition,
 });
 
+interface CroppedImage {
+  image: File,
+  crop?: Crop
+}
+
 function Petition() {
-  const [petitionFiles, setPetitionFiles] = useState<File[]>([]);
+  const [petitionFiles, setPetitionFiles] = useState<CroppedImage[]>([]);
+  const [selectedPetition, setSelectedPetition] = useState(-1); //-1 meaning nothing is selected
 
   const handlePetitionFilesAdded = (files: FileList) => {
-    let existingNames = petitionFiles.map(file => file.name);
-    let newFiles = Array.from(files).filter(file => existingNames.indexOf(file.name) === -1);
-    setPetitionFiles([
-      ...newFiles,
-      ...petitionFiles
-    ])
+    let existingNames = petitionFiles.map(file => file.image.name);
+    let newFiles: CroppedImage[] = Array.from(files).filter(file => existingNames.indexOf(file.name) === -1).map(file => ({ image: file }))
+    //need to convert pdfs to images here
+    if (newFiles.length > 0) {
+      setPetitionFiles(petitionFiles.concat(newFiles))
+      if (selectedPetition === -1) {
+        setSelectedPetition(0);
+      }
+    }
   };
 
   return (
@@ -55,10 +65,26 @@ Ensure these sections have the printed name and address of the voter.
           <FileInput id="petition-signatures" multiple onFilesAdded={handlePetitionFilesAdded} style={{ display: 'none' }} />
           <ul>
             {petitionFiles.map(file => (
-              <li>{file.name}</li>
+              <li key={file.image.name}>{file.image.name}</li>
             ))}
           </ul>
         </div>
+      </div>
+      <div>
+        {selectedPetition !== -1 && petitionFiles[selectedPetition] &&
+          <ReactCrop crop={petitionFiles[selectedPetition].crop} onChange={c => {
+            let oldCrop = petitionFiles[selectedPetition];
+            let files = [...petitionFiles];
+            files[selectedPetition] = {
+              image: oldCrop.image,
+              crop: c
+            }
+            setPetitionFiles(files);
+          }}>
+            <img src={URL.createObjectURL(petitionFiles[selectedPetition].image)} />
+
+          </ReactCrop>
+        }
       </div>
       <div className="border-gray-600 border-t-2 my-5">
         <h3>Process Files</h3>
